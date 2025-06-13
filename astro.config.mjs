4cf4e9c0e253a614878a0baebff276d59b261ab1
Tucker <tuckerhoog@tutanota.com>
2025-06-12 21:23:15 -0400

    try new site value

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/Grahf0085',
   vite: {
     plugins: [tailwindcss()],
   },
 })
-
